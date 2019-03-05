46afb77137d981922bf49c9072248de5f3c58144
Eric Rini <erini@itron.com>
2019-03-05 00:25:40 -0500

    Remove unnecessary comment.

--- src/controllers/valuation/ValuationController.js
+++ src/controllers/valuation/ValuationController.js
@@ -11,12 +11,6 @@ module.exports = class ValuationController {
         let responseUtil = new ResponseUtil(response);
 
         if (!validatePostRequestSchema(request.body)) {
-
-            // BRIEF EDITORIAL:
-            // In V8, all the user code runs on a single thread, so even though lines 13 and 20 are technically a
-            // critical section, it works anyway. While this is a strength that makes it possible to write performant
-            // IO parallelism in record time, it's also a liability that makes it impossible to solve CPU bound
-            // problems. For this reason it's hard for me to respect V8 as a "general purpose" platform. :)
             return responseUtil.sendAjvError(validatePostRequestSchema.errors);
         }
 
